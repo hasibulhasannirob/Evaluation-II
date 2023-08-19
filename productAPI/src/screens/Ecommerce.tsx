c224c49ff537f943bf1b6782d91ec282a9a078cc
hasibulhasannirob <hasibulnirob987@gmail.com>
2023-08-19 09:46:23 +0600

    Calling API and render all the product

--- productAPI/src/screens/Ecommerce.tsx
+++ productAPI/src/screens/Ecommerce.tsx
@@ -1,57 +1,88 @@
-import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, Image, StyleSheet } from 'react-native';
+import axios from 'axios';
 
-const Ecommerce = () => {
+const API_URL = 'https://fakestoreapi.com/products';
+
+const App = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(API_URL);
+      setProducts(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const renderItem = ({ item }) => (
+    <View style={styles.productContainer}>
+      <Image source={{ uri: item.image }} style={styles.productImage} />
+      <Text style={styles.productTitle}>{item.title}</Text>
+      <Text style={styles.productPrice}>Price: ${item.price}</Text>
+    </View>
+  );
+
   return (
-    <SafeAreaView style={styles.mainContainer}>
-    <View>
-        <Text style={{ fontWeight: "bold", fontSize: 25, marginBottom: 50, }}>Ecommerce</Text>
+    <View style={styles.container}>
+      <Text style={styles.header}>Ecommerce</Text>
+      <FlatList
+        data={products}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()}
+        contentContainerStyle={styles.listContainer}
+      />
     </View>
-
-    <View style={{ alignItems: "center", width: 300, height: 150, backgroundColor: "#bdfced", borderRadius: 5, flexDirection: "row", padding: 15, marginBottom: 10 }}>
-        <View style={{ alignItems: "center", justifyContent: "space-between" }}>
-            <Image
-               
-                style={{ width: 100, height: 100, }}
-            />
-        </View>
-        <View style={{ marginLeft: 10, }}>
-            <Text style={{ fontSize: 20, fontWeight: "bold" }}>Tide Powder</Text>
-            <Text>Washing Powder</Text>
-            <Text style={{ marginBottom: 10, }}>800gm</Text>
-            <TouchableOpacity style={{ alignItems: "center", justifyContent: "center" }}>
-                <Text style={{ borderWidth: 2, borderColor: "#27f5c1", borderRadius: 4, padding: 4, fontWeight: "bold" }}>Add To Bag</Text>
-            </TouchableOpacity>
-        </View>
-    </View>
-
-    <View style={{ alignItems: "center", width: 300, height: 150, backgroundColor: "#bdfced", borderRadius: 5, flexDirection: "row", padding: 15, marginBottom: 10 }}>
-        <View style={{ alignItems: "center", justifyContent: "space-between" }}>
-            <Image
-                
-                style={{ width: 100, height: 100, }}
-            />
-        </View>
-        <View style={{ marginLeft: 10, }}>
-            <Text style={{ fontSize: 20, fontWeight: "bold" }}>Onion</Text>
-            <Text>Daily Needs</Text>
-            <Text style={{ marginBottom: 10, }}>1kg</Text>
-            <TouchableOpacity style={{ alignItems: "center", justifyContent: "center" }}>
-                <Text style={{ borderWidth: 2, borderColor: "#27f5c1", borderRadius: 4, padding: 4, fontWeight: "bold" }}>Add To Bag</Text>
-            </TouchableOpacity>
-        </View>
-    </View>
-
-</SafeAreaView>
-  )
-}
-
-export default Ecommerce
+  );
+};
 
 const styles = StyleSheet.create({
-    mainContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
-})
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#f0f0f0',
+    alignItems: "center",
+  },
+  header: {
+    marginTop: 40,
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  listContainer: {
+    paddingBottom: 20,
+  },
+  productContainer: {
+    marginBottom: 20,
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    padding: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  productImage: {
+    width: '100%',
+    height: 200,
+    resizeMode: 'cover',
+    borderRadius: 8,
+  },
+  productTitle: {
+    fontSize: 18,
+    marginTop: 10,
+  },
+  productPrice: {
+    fontSize: 16,
+    color: '#888',
+    marginTop: 5,
+  },
+});
+
+export default App;
